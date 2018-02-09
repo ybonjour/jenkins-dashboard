358bd9070103768ad3651e0055b5f3d0b6d9d4b1
Yves Bonjour <yves.bonjour@ergon.ch>
2018-02-09 11:51:19 +0100

    Reformats dev-start.sh

--- dev-start.sh
+++ dev-start.sh
@@ -6,4 +6,8 @@ set -o nounset
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-docker run --rm -d --name dev-server -v "${dir}/src":/var/www -v "${dir}/nginx-conf/nginx.conf":/etc/nginx/nginx.conf -v "${dir}/nginx-conf/sites-available/jenkins-dashboard.conf":/etc/nginx/sites-enabled/jenkins-dashboard.conf -p 8080:80 nginx
+docker run --rm -d --name dev-server \
+-v "${dir}/src":/var/www \
+-v "${dir}/nginx-conf/nginx.conf":/etc/nginx/nginx.conf \
+-v "${dir}/nginx-conf/sites-available/jenkins-dashboard.conf":/etc/nginx/sites-enabled/jenkins-dashboard.conf \
+-p 8080:80 nginx
