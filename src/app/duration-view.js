4878ee363fa0b99e2c6339ff1e245d7275467ccc
Yves Bonjour <yves.bonjour@ergon.ch>
2018-02-10 18:01:56 +0100

    Extracts duration into DurationView

---
+++ src/app/duration-view.js
@@ -0,0 +1,64 @@
+import {getJobOverview, getJobDetails} from "./jenkins-api.js"
+
+export class DurationView extends HTMLElement {
+    constructor() {
+        super();
+        let that = this;
+        getJobOverview().then(function(jobOverview) {
+            let buildNumber;
+            let style;
+            if (that.isCurrentlySuccessful(jobOverview)) {
+                buildNumber = jobOverview.lastUnsuccessfulBuild;
+                style = "successful";
+            } else {
+                buildNumber = jobOverview.lastUnsuccessfulBuild;
+                style = "unsuccessful";
+            }
+            
+            that.getDurationText(buildNumber, jobOverview.lastCompletedBuild)
+                .then(function(text) {
+                    that.render(text, style)
+                });
+        });
+    }
+
+    render(text, style) {
+        this.innerHTML = `
+            <h1 class="${style}">
+                ${text}
+            </h1>`;
+    }
+    
+    isCurrentlySuccessful(jobOverview) {
+        return jobOverview.lastSuccessfulBuild > jobOverview.lastUnsuccessfulBuild
+    }
+    
+    getDurationText(buildNumber, lastBuildNumber) {
+        return this.sinceText(buildNumber)
+            .then(function(period) {
+                const numBuilds = lastBuildNumber - buildNumber;
+                return `${period} (${numBuilds} Builds)`
+            });
+    }
+
+    sinceText(buildNumber) {
+        let that = this;
+        return getJobDetails(buildNumber)
+            .then(function(job) {
+                return that.periodMinutes(Date.now() - job.timestamp);
+            });
+    }
+
+    periodMinutes(period) {
+        let diff = period;
+        const hours = Math.floor(diff / (1000*60*60));
+        diff = diff % (1000*60*60);
+        const minutes = Math.floor(diff / (1000*60));
+        let hoursOutput = ""
+        if(hours > 0) {
+            hoursOutput += `${hours} h `
+        }
+        return `${hoursOutput}${minutes} min`;
+    }
+
+}
--- src/app/main.js
+++ src/app/main.js
@@ -1,53 +1,6 @@
 import {getJobOverview, getJobDetails, getWorkflow} from "./jenkins-api.js"
 import {PipelineView} from "./pipeline-view.js"
+import {DurationView} from "./duration-view.js"
 
 customElements.define("pipeline-view", PipelineView);
-
-getJobOverview()
-    .then(function(jobOverview) {
-        const durationElement = document.getElementById("duration");
-        let buildNumber;
-        let cssClass;
-        if (isCurrentlySuccessful(jobOverview)) {
-            buildNumber = jobOverview.lastUnsuccessfulBuild;
-            durationElement.className = "successful";
-        } else {
-            buildNumber = jobOverview.lastUnsuccessfulBuild;
-            durationElement.className = "unsuccessful";
-        }
-        getDurationText(buildNumber, jobOverview.lastCompletedBuild)
-            .then(function(text) {
-                durationElement.innerHTML = text;
-            });
-    });
-
-function isCurrentlySuccessful(jobOverview) {
-    return jobOverview.lastSuccessfulBuild > jobOverview.lastUnsuccessfulBuild
-}
-
-function getDurationText(buildNumber, lastBuildNumber) {
-    return sinceText(buildNumber)
-        .then(function(period) {
-            const numBuilds = lastBuildNumber - buildNumber;
-            return `${period} (${numBuilds} Builds)`
-        });
-}
-
-function sinceText(buildNumber) {
-    return getJobDetails(buildNumber)
-        .then(function(job) {
-            return periodMinutes(job.timestamp, Date.now());
-        });
-}
-
-function periodMinutes(from, to) {
-    let diff = to - from;
-    const hours = Math.floor(diff / (1000*60*60));
-    diff = diff % (1000*60*60);
-    const minutes = Math.floor(diff / (1000*60));
-    let hoursOutput = ""
-    if(hours > 0) {
-        hoursOutput += `${hours} h `
-    }
-    return `${hoursOutput}${minutes} min`;
-}
+customElements.define("duration-view", DurationView);
