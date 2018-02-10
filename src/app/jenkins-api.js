677ab206d9d1b83443f8c9aad12f6651b12a6830
Yves Bonjour <yves.bonjour@ergon.ch>
2018-02-10 14:23:07 +0100

    Adds styles and basic pipeline view

--- src/app/jenkins-api.js
+++ src/app/jenkins-api.js
@@ -1,4 +1,66 @@
-function parseJobInfo(json) {
+const jobOverview = "job/coop.mws.pipeline-ERGOneo/job/master"
+
+export function getJobOverview(){
+    return fetch(jenkinsEndpoint(jobOverview))
+        .then(jsonBody)
+        .then(parseJobOverview);
+}
+
+export function getJobDetails(buildNumber) {
+    return fetch(jenkinsEndpoint(`${jobOverview}/${buildNumber}`))
+    .then(jsonBody)
+    .then(parseJobDetails)
+}
+
+export function getWorkflow(buildNumber) {
+    return fetch(workflowEndpoint(jobOverview, `runs?since=%23${buildNumber}`))
+        .then(jsonBody)
+        .then(selectJob(buildNumber))
+        .then(parseWorkflow)
+}
+
+function selectJob(buildNumber) {
+    return function(json) {
+        for(let elementIdx in json){
+            const element = json[elementIdx];
+            if(Number(element["id"]) == buildNumber) {
+                return element;
+            }
+        }
+        return {};
+    }
+}
+
+function workflowEndpoint(path, workflowEndpoint) {
+    return `jenkins/${path}/wfapi/${workflowEndpoint}`;
+}
+
+function parseWorkflow(json) {
+    return {
+        buildNumber: Number(json["id"]),
+        stages: parseStages(json["stages"])       
+    }
+}
+
+function parseStages(json) {
+    let stages = [];
+    for(let stageIdx in json) {
+        const stage = json[stageIdx];
+        stages.push({
+            name: stage["name"],
+            status: stage["status"],
+            durationMs: Number(stage["durationMillis"])
+        });
+    }
+    
+    return stages;
+}
+
+function jenkinsEndpoint(path) {
+    return `jenkins/${path}/api/json`
+}
+
+function parseJobOverview(json) {
     return {
         lastSuccessfulBuild: json["lastSuccessfulBuild"]["number"],
         lastUnsuccessfulBuild: json["lastUnsuccessfulBuild"]["number"],
@@ -6,20 +68,12 @@ function parseJobInfo(json) {
     };
 }
 
-export function getJobInfo(){
-    return fetch('jenkins/job/coop.mws.pipeline-ERGOneo/job/master/api/json')
-        .then(function(response) { return response.json(); })
-        .then(parseJobInfo);
+function parseJobDetails(json) {
+    return {
+        timestamp: json["timestamp"]
+    }
 }
 
-export function isCurrentlySuccessful(jobInfo) {
-    return jobInfo.lastSuccessfulBuild > jobInfo.lastUnsuccessfulBuild
-}
-
-export function numberOfSuccessfulBuilds(jobInfo) {
-    return jobInfo.lastCompletedBuild - jobInfo.lastUnsuccessfulBuild
-}
-
-export function numberOfUnsuccessfuldBuilds(jobInfo) {
-    return jobInfo.lastCompletedBuild - jobInfo.lastSuccessfulBuild
+function jsonBody(response) {
+    return response.json();
 }
--- src/app/main.js
+++ src/app/main.js
@@ -1,16 +1,85 @@
-import {getJobInfo, isCurrentlySuccessful, numberOfSuccessfulBuilds} from './jenkins-api'
+import {getJobOverview, getJobDetails, getWorkflow} from './jenkins-api'
 import $ from 'jquery'
 
-getJobInfo()
-   .then(function(jobinfo) {
-        let text;
-        if (isCurrentlySuccessful(jobinfo)) {
-            text = `All good since ${numberOfSuccessfulBuilds(jobinfo)} builds`
+getJobOverview()
+    .then(function(jobOverview) {
+        let buildNumber;
+        let cssClass;
+        if (isCurrentlySuccessful(jobOverview)) {
+            buildNumber = jobOverview.lastUnsuccessfulBuild;
+            $("#duration").removeClass("unsuccessful");
+            $("#duration").addClass("successful");
         } else {
-            text = `Failing since ${numberOfUnsuccessfulBuilds(jobinfo)} builds`
+            buildNumber = jobOverview.lastUnsuccessfulBuild;
+            $("#duration").removeClass("successful");
+            $("#duration").addClass("unsuccessful");
         }
-        $("#content").text(text)
-    })
-   .catch(function(error) {
-        $("#error").text(JSON.stringify(error));
+        getDurationText(buildNumber, jobOverview.lastCompletedBuild)
+            .then(function(text) {
+                $("#duration").text(text);
+            });
+    
+        getPipeline(jobOverview.lastCompletedBuild);
     });
+
+function renderPipeline(pipeline) {
+    let output = ""
+    for(let stageIdx in pipeline.stages) {
+        const stage = pipeline.stages[stageIdx];
+        output += `<div class="stage">${stage.name}<br/>(${periodSeconds(stage.durationMs)})</div>`
+    }
+    return output;
+}
+
+
+function getPipeline(buildNumber) {
+    getWorkflow(buildNumber)
+        .then(function(pipeline) {
+            console.log(JSON.stringify(pipeline));
+            $("#pipeline").html(renderPipeline(pipeline));
+        });
+}
+
+function isCurrentlySuccessful(jobOverview) {
+    return jobOverview.lastSuccessfulBuild > jobOverview.lastUnsuccessfulBuild
+}
+
+function getDurationText(buildNumber, lastBuildNumber) {
+    return sinceText(buildNumber)
+        .then(function(period) {
+            const numBuilds = lastBuildNumber - buildNumber;
+            return `${period} (${numBuilds} Builds)`
+        });
+}
+
+function sinceText(buildNumber) {
+    return getJobDetails(buildNumber)
+        .then(function(job) {
+            return periodMinutes(job.timestamp, Date.now());
+        });
+}
+
+function periodSeconds(period) {
+    let diff = period;
+    const minutes = Math.floor(diff / (1000*60));
+    diff = diff % (1000*60);
+    const seconds = Math.floor(diff / 1000)
+    let minutesOutput = ""    
+    if(minutes > 0) {
+        minutesOutput += `${minutes} min `
+    }
+    return `${minutesOutput}${seconds} sec`
+    
+}
+
+function periodMinutes(from, to) {
+    let diff = to - from;
+    const hours = Math.floor(diff / (1000*60*60));
+    diff = diff % (1000*60*60);
+    const minutes = Math.floor(diff / (1000*60));
+    let hoursOutput = ""
+    if(hours > 0) {
+        hoursOutput += `${hours} h `
+    }
+    return `${hoursOutput}${minutes} min`;
+}
